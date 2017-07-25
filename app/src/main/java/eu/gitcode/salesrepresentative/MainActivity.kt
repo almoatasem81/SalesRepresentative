8472688d7786d528d9d7d4a6dcab26344981cc98
marcin.grabowski <marcingrabowski91@gmail.com>
2017-07-25 17:47:23 +0200

    Customize main activity bar

--- app/src/main/java/eu/gitcode/salesrepresentative/MainActivity.kt
+++ app/src/main/java/eu/gitcode/salesrepresentative/MainActivity.kt
@@ -7,28 +7,38 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavigationListener()
+    }
 
+    private fun setupNavigationListener() {
+        val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_orders -> {
+                    message.setText(R.string.orders)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_shops -> {
+                    message.setText(R.string.shops)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_goods -> {
+                    message.setText(R.string.goods)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_notes -> {
+                    message.setText(R.string.notes)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_timetable -> {
+                    message.setText(R.string.timetable)
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
